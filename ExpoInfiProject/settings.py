9607e4ec79133f8c08a6b1274e705e288d2650ce
manask322 <manask322@gmail.com>
2020-04-02 22:01:23 +0530

    debug

--- ExpoInfiProject/settings.py
+++ ExpoInfiProject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '*sb0@&n^drr^jrpo2c765t67d%ji@mrun&))ek6g53ege5ah#$')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 # AUTH_USER_MODEL = 'users.CustomUser'
